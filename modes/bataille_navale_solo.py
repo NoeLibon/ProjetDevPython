b01ea8365c4f2589646bbe076b6c8666027e8425
julienmeire <71373002+julienmeire@users.noreply.github.com>
2022-12-04 18:22:54 +0100

    Update bataille_navale_solo.py

--- modes/bataille_navale_solo.py
+++ modes/bataille_navale_solo.py
@@ -31,6 +31,16 @@ class ModeSolo(BatailleNavale):
             j.tir(o)
             if self.flotte_coule(o) is True:
                 self.message_victorieux(j, o)
+                sqlite_file = (r"db.sqlite")
+                connection = sqlite3.connect(sqlite_file)
+                cursor = connection.cursor()
+                sql = "CREATE TABLE IF NOT EXISTS " + "db" + "(id integer PRIMARY KEY AUTOINCREMENT, result char not null)"
+                cursor.execute(sql)
+                sql = "INSERT INTO " + "db" + "(result) VALUES (\"self.message_victorieux(j, o)\")"
+                cursor.execute(sql)
+
+                connection.commit()
+                cursor.close()
                 drapeau = False
             else:
                 self.clear_screen()
@@ -38,6 +48,16 @@ class ModeSolo(BatailleNavale):
                 o.frappe_ennemie(j)
                 if self.flotte_coule(j) is True:
                     self.message_victorieux(o, j)
+                    sqlite_file = (r"db.sqlite")
+                    connection = sqlite3.connect(sqlite_file)
+                    cursor = connection.cursor()
+                    sql = "CREATE TABLE IF NOT EXISTS " + "db" + "(id integer PRIMARY KEY AUTOINCREMENT, result char not null)"
+                    cursor.execute(sql)
+                    sql = "INSERT INTO " + "db" + "(result) VALUES (\"self.message_victorieux(o, j)\")"
+                    cursor.execute(sql)
+
+                connection.commit()
+                cursor.close()
                     drapeau = False
                 else:
                     self.clear_screen()
