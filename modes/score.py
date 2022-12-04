c4c8083a5713f3d486dd5666934faa6f5ec4495f
julienmeire <71373002+julienmeire@users.noreply.github.com>
2022-12-04 16:05:40 +0100

    Update score.py

--- modes/score.py
+++ modes/score.py
@@ -3,10 +3,13 @@ import sqlite3
 class Score():
 
     def __init__(self):
-        sqlite_file = (r"C:\Users\32498\sqlite3\data.db")
+        sqlite_file = (r"db.sqlite")
         connection = sqlite3.connect(sqlite_file)
         cursor = connection.cursor()
-        sql = "SELECT * FROM " + "db"
+        sql = "CREATE TABLE IF NOT EXISTS " + "db" + "(id integer PRIMARY KEY AUTOINCREMENT, result char not null)"
         cursor.execute(sql)
+        sql = "SELECT * FROM " + "db"
+        a = cursor.execute(sql).fetchall()
+        print(a)
         connection.commit()
         cursor.close()
